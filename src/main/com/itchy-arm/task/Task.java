09357cf96bd153974be219dfc8673d28ac926e70
Tim Renouf <tim.renouf@gmail.com>
2013-01-17 11:58:12 +1300

    Oops, not allowed '-' in package names

--- src/main/com/itchy-arm/task/Task.java
+++
@@ -1,9 +0,0 @@
-package com.itchyarm.task;
-
-import java.util.Date;
-
-public interface Task {
-	public void setName( String name );
-	public void setLastDate();
-	public void setLastDate( Date date );
-}
