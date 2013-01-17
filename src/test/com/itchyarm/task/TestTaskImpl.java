ae59026ed32f37326c1aec999e16e2aef9f17300
Tim Renouf <tim.renouf@gmail.com>
2013-01-17 17:43:07 +1300

    Refining .gitignore files

--- src/test/com/itchyarm/task/TestTaskImpl.java
+++ src/test/com/itchyarm/task/TestTaskImpl.java
@@ -2,6 +2,8 @@ package com.itchyarm.task;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 public class TestTaskImpl {
@@ -9,8 +11,15 @@ public class TestTaskImpl {
 	@Test
 	public void testLifeCycle() {
 		Task task = new TaskImpl();
-		String name = task.getName();
-		task.setName("Testing");
-		assertEquals("Setting task name doesn't seem to work.", name, task.getName());
+		
+		// Check the name getter and setter
+		String taskName = "Oil change";
+		task.setName(taskName);
+		assertEquals("Setting task name doesn't seem to work.", taskName, task.getName());
+		
+		// Check the date getter and setter
+		Date taskDate = new Date();
+		task.setLastDate(taskDate);
+		assertEquals("Setting task date doesn't seem to work.", taskDate, task.getLastDate());
 	}
 }
