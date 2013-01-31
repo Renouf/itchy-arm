4e36258a41bc2301a732e46c790ddc28a338d0ec
Tim Renouf <tim.renouf@gmail.com>
2013-02-01 12:57:00 +1300

    Tidying up Launcher.

--- src/main/java/com/itchyarm/task/Launcher.java
+++ src/main/java/com/itchyarm/task/Launcher.java
@@ -11,8 +11,7 @@ public class Launcher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Task task = new TaskImpl();
-		task.setName("Logging test");
+		logger.debug("Launching itchy-arm application.");
 	}
 
 }
